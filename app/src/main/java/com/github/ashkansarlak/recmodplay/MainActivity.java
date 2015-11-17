e83c755b33eee15b676518cbda666f4f5ca91794
Ashkan Sarlak <ashkan.sarlak@gmail.com>
2015-11-17 19:56:19 +0330

    music volumes changes with environment volume

--- app/src/main/java/com/github/ashkansarlak/recmodplay/MainActivity.java
+++ app/src/main/java/com/github/ashkansarlak/recmodplay/MainActivity.java
@@ -4,10 +4,12 @@ import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
+import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -24,6 +26,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,9 +35,10 @@ public class MainActivity extends AppCompatActivity {
     private ArrayAdapter<Integer> adapter;
 
     Spinner spFrequency;
-    Button startRec, stopRec, playBack;
+    Button startRec, stopRec, playBack, recAndPlay;
 
     Boolean recording;
+    private MediaPlayer mediaPlayer;
 
     /**
      * Called when the activity is first created.
@@ -46,16 +50,17 @@ public class MainActivity extends AppCompatActivity {
         startRec = (Button) findViewById(R.id.startrec);
         stopRec = (Button) findViewById(R.id.stoprec);
         playBack = (Button) findViewById(R.id.playback);
+        recAndPlay = (Button) findViewById(R.id.recAndPlay);
 
         startRec.setOnClickListener(startRecOnClickListener);
         stopRec.setOnClickListener(stopRecOnClickListener);
         playBack.setOnClickListener(playBackOnClickListener);
+        recAndPlay.setOnClickListener(recAndPlayOnClickListener);
 
         spFrequency = (Spinner) findViewById(R.id.frequency);
         adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, freqset);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spFrequency.setAdapter(adapter);
-
     }
 
     View.OnClickListener startRecOnClickListener
@@ -75,7 +80,6 @@ public class MainActivity extends AppCompatActivity {
             });
 
             recordThread.start();
-
         }
     };
 
@@ -97,6 +101,83 @@ public class MainActivity extends AppCompatActivity {
         }
 
     };
+
+    private View.OnClickListener recAndPlayOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Thread recAndPlayThread = new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+                    recording = true;
+                    recAndPlay();
+                }
+
+            });
+
+            recAndPlayThread.start();
+        }
+    };
+
+    private void recAndPlay() {
+        int shortSizeInBytes = Short.SIZE / Byte.SIZE;
+        int sampleFreq = (Integer) spFrequency.getSelectedItem();
+        int minBufferSize = AudioRecord.getMinBufferSize(sampleFreq,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        short[] audioData = new short[minBufferSize];
+
+        AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                sampleFreq,
+                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.ENCODING_PCM_16BIT,
+                minBufferSize);
+
+//        AudioTrack audioTrack = new AudioTrack(
+//                AudioManager.STREAM_MUSIC,
+//                sampleFreq,
+//                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+//                AudioFormat.ENCODING_PCM_16BIT,
+//                shortSizeInBytes,
+//                AudioTrack.MODE_STREAM);
+
+        audioRecord.startRecording();
+        startMusic();
+//        audioTrack.play();
+        int offset = 0;
+
+        while (recording) {
+            int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
+//            audioTrack.write(audioData, offset, numberOfShort);
+            offset += numberOfShort;
+            Log.v("AUDIO_DATA", Arrays.toString(audioData));
+            double gain = getGain(audioData, numberOfShort);
+            Log.v("GAIN", String.valueOf(gain));
+            mediaPlayer.setVolume((float) gain / 2000, (float) gain / 2000);
+        }
+
+        audioRecord.stop();
+        stopMusic();
+//        audioTrack.stop();
+    }
+
+    private void stopMusic() {
+        mediaPlayer.stop();
+    }
+
+    private void startMusic() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        mediaPlayer.start();
+    }
+
+    private double getGain(short[] audioData, int numOfShort) {
+        double sumLevel = 0;
+        for (int i = 0; i < numOfShort; i++) {
+            sumLevel += Math.abs(audioData[i]);
+        }
+        return Math.abs((sumLevel / numOfShort));
+    }
 
     private void startRecord() {
 
