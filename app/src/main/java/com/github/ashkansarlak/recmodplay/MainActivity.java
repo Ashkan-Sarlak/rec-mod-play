f00cf8cccce417373d5439ff2dccf9c4b361064c
Ashkan Sarlak <ashkan.sarlak@gmail.com>
2015-11-18 12:12:29 +0330

    demo version: out volume smoothly changes with env volume

--- app/src/main/java/com/github/ashkansarlak/recmodplay/MainActivity.java
+++ app/src/main/java/com/github/ashkansarlak/recmodplay/MainActivity.java
@@ -1,19 +1,29 @@
 package com.github.ashkansarlak.recmodplay;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioRecord;
 import android.media.AudioTrack;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.OpenableColumns;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -31,14 +41,16 @@ import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    Integer[] freqset = {11025, 16000, 22050, 44100};
-    private ArrayAdapter<Integer> adapter;
+    private static final int SAMPLING_FREQUENCY = 44100;
+    private final static int RQS_OPEN_AUDIO_MP3 = 1;
 
-    Spinner spFrequency;
-    Button startRec, stopRec, playBack, recAndPlay;
-
-    Boolean recording;
+    private FloatingActionButton recAndPlay;
+    private boolean recording;
     private MediaPlayer mediaPlayer;
+    private SeekBar followingFactorSeekbar;
+    private ProgressBar envVol, headVol;
+    private ImageButton browseAudio;
+    private TextView songName;
 
     /**
      * Called when the activity is first created.
@@ -47,119 +59,137 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startRec = (Button) findViewById(R.id.startrec);
-        stopRec = (Button) findViewById(R.id.stoprec);
-        playBack = (Button) findViewById(R.id.playback);
-        recAndPlay = (Button) findViewById(R.id.recAndPlay);
 
-        startRec.setOnClickListener(startRecOnClickListener);
-        stopRec.setOnClickListener(stopRecOnClickListener);
-        playBack.setOnClickListener(playBackOnClickListener);
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        recAndPlay = (FloatingActionButton) findViewById(R.id.recAndPlay);
         recAndPlay.setOnClickListener(recAndPlayOnClickListener);
-
-        spFrequency = (Spinner) findViewById(R.id.frequency);
-        adapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, freqset);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spFrequency.setAdapter(adapter);
+        followingFactorSeekbar = (SeekBar) findViewById(R.id.followingFactor);
+        envVol = (ProgressBar) findViewById(R.id.envVol);
+        headVol = (ProgressBar) findViewById(R.id.headVol);
+        browseAudio = (ImageButton) findViewById(R.id.browseAudio);
+        browseAudio.setOnClickListener(browseAudioClickListener);
+        songName = (TextView) findViewById(R.id.songName);
     }
 
-    View.OnClickListener startRecOnClickListener
-            = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View arg0) {
-
-            Thread recordThread = new Thread(new Runnable() {
-
-                @Override
-                public void run() {
-                    recording = true;
-                    startRecord();
-                }
-
-            });
-
-            recordThread.start();
-        }
-    };
-
-    View.OnClickListener stopRecOnClickListener
-            = new View.OnClickListener() {
-
-        @Override
-        public void onClick(View arg0) {
-            recording = false;
-        }
-    };
-
-    View.OnClickListener playBackOnClickListener
-            = new View.OnClickListener() {
-
+    private View.OnClickListener browseAudioClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            playRecord();
-        }
+            if (recording) return;
 
+            Intent intent = new Intent();
+            intent.setType("audio/mp3");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(
+                    intent, "Select an MP3 file to play through headphones"), RQS_OPEN_AUDIO_MP3);
+
+        }
     };
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == RQS_OPEN_AUDIO_MP3) {
+                Uri audioFileUri = data.getData();
+                mediaPlayer = MediaPlayer.create(this, audioFileUri);
+                songName.setText(getFileName(audioFileUri));
+            }
+        }
+    }
+
+    private String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
 
     private View.OnClickListener recAndPlayOnClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Thread recAndPlayThread = new Thread(new Runnable() {
+            if (recording) {
+                recording = false;
+                recAndPlay.setImageResource(R.drawable.ic_play_arrow_white_18dp);
+            } else {
+                recAndPlay.setImageResource(R.drawable.ic_stop_white_18dp);
+                Thread recAndPlayThread = new Thread(new Runnable() {
 
-                @Override
-                public void run() {
-                    recording = true;
-                    recAndPlay();
-                }
+                    @Override
+                    public void run() {
+                        recording = true;
+                        recAndPlay();
+                    }
 
-            });
+                });
 
-            recAndPlayThread.start();
+                recAndPlayThread.start();
+            }
         }
     };
 
     private void recAndPlay() {
-        int shortSizeInBytes = Short.SIZE / Byte.SIZE;
-        int sampleFreq = (Integer) spFrequency.getSelectedItem();
+        int sampleFreq = SAMPLING_FREQUENCY;
         int minBufferSize = AudioRecord.getMinBufferSize(sampleFreq,
-                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT);
 
         short[] audioData = new short[minBufferSize];
 
         AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
                 sampleFreq,
-                AudioFormat.CHANNEL_CONFIGURATION_MONO,
+                AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT,
                 minBufferSize);
 
-//        AudioTrack audioTrack = new AudioTrack(
-//                AudioManager.STREAM_MUSIC,
-//                sampleFreq,
-//                AudioFormat.CHANNEL_CONFIGURATION_MONO,
-//                AudioFormat.ENCODING_PCM_16BIT,
-//                shortSizeInBytes,
-//                AudioTrack.MODE_STREAM);
-
         audioRecord.startRecording();
         startMusic();
-//        audioTrack.play();
-        int offset = 0;
 
         while (recording) {
             int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
-//            audioTrack.write(audioData, offset, numberOfShort);
-            offset += numberOfShort;
             Log.v("AUDIO_DATA", Arrays.toString(audioData));
             double gain = getGain(audioData, numberOfShort);
-            Log.v("GAIN", String.valueOf(gain));
-            mediaPlayer.setVolume((float) gain / 2000, (float) gain / 2000);
+            Log.v("GAIN", String.valueOf(gain / 2000));
+            setVolumeTarget(gain / 2000);
+            showVolume(envVol, gain/ 2000);
         }
 
         audioRecord.stop();
         stopMusic();
-//        audioTrack.stop();
+    }
+
+    private void showVolume(ProgressBar volume, double v) {
+        volume.setProgress((int) ((v > 1 ? 1 : v) * 100));
+    }
+
+    private float currentVolume = 0;
+
+    private void setVolumeTarget(double targetVolume) {
+        if (mediaPlayer == null) return;
+
+        currentVolume += (targetVolume - currentVolume) / getFollowingFactor();
+        mediaPlayer.setVolume(currentVolume, currentVolume);
+        showVolume(headVol, currentVolume);
+        Log.v("HEADPHONE_GAIN", String.valueOf(currentVolume));
+    }
+
+    private double getFollowingFactor() {
+        int factor = followingFactorSeekbar.getProgress();
+        return factor < 1 ? 1 : factor;
     }
 
     private void stopMusic() {
@@ -167,7 +197,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startMusic() {
-        mediaPlayer = MediaPlayer.create(this, R.raw.music);
+        currentVolume = 0;
         mediaPlayer.start();
     }
 
@@ -179,90 +209,4 @@ public class MainActivity extends AppCompatActivity {
         return Math.abs((sumLevel / numOfShort));
     }
 
-    private void startRecord() {
-
-        File file = new File(getCacheDir(), "test.pcm");
-
-        int sampleFreq = (Integer) spFrequency.getSelectedItem();
-
-        try {
-            file.createNewFile();
-
-            OutputStream outputStream = new FileOutputStream(file);
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
-            DataOutputStream dataOutputStream = new DataOutputStream(bufferedOutputStream);
-
-            int minBufferSize = AudioRecord.getMinBufferSize(sampleFreq,
-                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT);
-
-            short[] audioData = new short[minBufferSize];
-
-            AudioRecord audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC,
-                    sampleFreq,
-                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    minBufferSize);
-
-            audioRecord.startRecording();
-
-            while (recording) {
-                int numberOfShort = audioRecord.read(audioData, 0, minBufferSize);
-                for (int i = 0; i < numberOfShort; i++) {
-                    dataOutputStream.writeShort(audioData[i]);
-                }
-            }
-
-            audioRecord.stop();
-            dataOutputStream.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    void playRecord() {
-
-        File file = new File(getCacheDir(), "test.pcm");
-
-        int shortSizeInBytes = Short.SIZE / Byte.SIZE;
-
-        int bufferSizeInBytes = (int) (file.length() / shortSizeInBytes);
-        short[] audioData = new short[bufferSizeInBytes];
-
-        try {
-            InputStream inputStream = new FileInputStream(file);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
-            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
-
-            int i = 0;
-            while (dataInputStream.available() > 0) {
-                audioData[i] = dataInputStream.readShort();
-                i++;
-            }
-
-            dataInputStream.close();
-
-            int sampleFreq = (Integer) spFrequency.getSelectedItem();
-
-            AudioTrack audioTrack = new AudioTrack(
-                    AudioManager.STREAM_MUSIC,
-                    sampleFreq,
-                    AudioFormat.CHANNEL_CONFIGURATION_MONO,
-                    AudioFormat.ENCODING_PCM_16BIT,
-                    bufferSizeInBytes,
-                    AudioTrack.MODE_STREAM);
-
-            audioTrack.play();
-            audioTrack.write(audioData, 0, bufferSizeInBytes);
-
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
-
